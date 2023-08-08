a636c7c41d9811c7852921b08b5860ac12f8b61b
kobisherm <kobisherman4@yahoo.com>
2023-08-07 19:52:53 -0700

    Added new navigation buttons to homepage

--- HomePage.java
+++ HomePage.java
@@ -15,17 +15,16 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-//TODO: FOLLOW TUTORIAL, ADD SEARCH BAR TEXT, LOGO, ETC...
 public class HomePage extends JFrame{
 	
+	final Font titleFont = new Font("SansSerif", Font.BOLD, 30);
+	final Font mainFont = new Font("SansSerif", Font.PLAIN, 20);
 	JButton searchButton;
 	
 	//Instead of display homepage
-	void HomePage()
+	void DisplayHomePage()
 	{
 		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		final Font titleFont = new Font("SansSerif", Font.BOLD, 30);
-		final Font mainFont = new Font("SansSerif", Font.PLAIN, 20);
 		ImageIcon logo = new ImageIcon("Logo.png");
 		
 		setTitle("Home page");
@@ -50,14 +49,22 @@ public class HomePage extends JFrame{
 		searchBarText.setPreferredSize(new Dimension(200, 40));
 		searchBarText.setFocusable(true);
 		
-		searchButton = new JButton();
-		searchButton.setFont(mainFont);
-		searchButton.setBackground(Color.GRAY);
-		searchButton.setForeground(Color.WHITE);
-		searchButton.setPreferredSize(new Dimension(200, 40));
-		searchButton.setText("Search");
-		searchButton.setFocusable(false);
-		searchButton.addActionListener(a -> System.out.println("pressed"));
+		searchButton = createStandardButton("Search...", new Dimension(200, 40));
+		searchButton.addActionListener(a -> onSearchClick());
+		
+		//To create a space
+		JLabel space = new JLabel();
+		space.setPreferredSize(new Dimension(400, 1));
+		space.setFocusable(false);
+		
+		JButton myCartButton = createStandardButton("My Cart", new Dimension(150, 40));
+		myCartButton.addActionListener(a -> onCartClick());
+		
+		JButton checkoutButton = createStandardButton("Checkout", new Dimension(150, 40));
+		checkoutButton.addActionListener(a -> onCheckoutClick());
+		
+		JButton LogOutButton = createStandardButton("Log out", new Dimension(150, 40));
+		LogOutButton.addActionListener(a -> onLogoutClick());
 		
 		//THE FOLLOWING CONTROLS THE UI UNDER THE TOP BAR
 		JPanel appLogoBar = new JPanel();
@@ -65,7 +72,7 @@ public class HomePage extends JFrame{
 		appLogoBar.setBackground(new Color(150, 0, 0));
 		appLogoBar.setLayout(new BorderLayout());
 		
-		JLabel appTitle = new JLabel("HI-LO");
+		JLabel appTitle = new JLabel(" HI-LO");
 		appTitle.setFont(titleFont);
 		appTitle.setForeground(Color.WHITE);
 		appTitle.setIcon(new ImageIcon(logo.getImage().getScaledInstance(125, 125, Image.SCALE_SMOOTH)));
@@ -75,11 +82,47 @@ public class HomePage extends JFrame{
 		topBar.add(homeLabel);
 		topBar.add(searchBarText);
 		topBar.add(searchButton);
+		topBar.add(space);
+		topBar.add(myCartButton);
+		topBar.add(checkoutButton);
+		topBar.add(LogOutButton);
 		
 		add(appLogoBar);
 		appLogoBar.add(appTitle);
 		
 		setResizable(false);
 		setVisible(true);
+	}
+	
+	private JButton createStandardButton(String text, Dimension size)
+	{
+		JButton btn = new JButton(text);
+		btn.setFont(mainFont);
+		btn.setBackground(Color.GRAY);
+		btn.setForeground(Color.WHITE);
+		btn.setPreferredSize(size);
+		btn.setFocusable(false);
+		return btn;
+	}
+	
+	// *********************** BUTTON EVENTS **************************************
+	public void onSearchClick()
+	{
+		System.out.println("pressed");
+	}
+	
+	public void onCartClick()
+	{
+		System.out.println("going to cart page");
+	}
+
+	public void onCheckoutClick()
+	{
+		System.out.println("going to checkout page");
+	}
+	
+	public void onLogoutClick()
+	{
+		System.out.println("Log out");
 	}
 }
