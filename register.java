8f5c734a56a12dce9ae079e6e379c847295251c0
Davit <sargsyan_davit@protonmail.com>
2023-08-08 18:41:24 -0700

    naming changes

--- register.java
+++ register.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class register extends JPanel {
+public class Register extends JPanel {
 	private JTextField emailField;
 	private JPasswordField passwordField;
 	private JButton registerButton;
--- user.java
+++ user.java
@@ -2,7 +2,7 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
-public class user {
+public class User {
 	private String userID;
 	private String email;
 	private String password;
