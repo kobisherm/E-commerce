a9f6c1362b023ddeff2a31d2be0d2f30ddda35ac
Icon-1999 <armonjlee99@gmail.com>
2023-08-13 01:56:43 -0700

    more order

--- Order.java
+++ Order.java
@@ -3,6 +3,13 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Order extends JFrame{
+    private static JLabel orderTxt, addressInfoTxt, cardInfoTxt;
+    private static JTextField address1, address2, zipcode, city, state;
+    private static JTextField cardName, cardNum, CVV;
+    private static JLabel address1Txt, address2Txt, zipcodeTxt, cityTxt, stateTxt;
+    private static JLabel cardNameTxt, cardNumTxt, CVVTxt;
+    private static JButton orderButton;
+
     public static void main(String[] args) {
         DisplayOrderScreen();
     }
@@ -20,6 +27,16 @@ public class Order extends JFrame{
         window.add(panel);
         window.setSize(new Dimension(700, 700));
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gbc.gridwidth = 1;
+
+        //H1 order
+        orderTxt = new JLabel("Order");
+        orderTxt.setForeground(AppTheme.black);
+        panel.add(orderTxt);
+
+        //address
+
+        //card
 
         window.setVisible(true);
     }
