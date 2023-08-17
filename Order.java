9c3d3fb634edc47af04ee37d0d7492f74dfb0427
Davit <sargsyan_davit@protonmail.com>
2023-08-16 21:38:22 -0700

    made changes, added design and button for purchase

--- Order.java
+++ Order.java
@@ -2,91 +2,109 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Order extends JFrame{
-    private static JLabel orderTxt, addressInfoTxt, cardInfoTxt;
-    private static JTextField address1, address2, zipcode, city, state;
-    private static JTextField cardName, cardNum, CVV;
-    private static JLabel addressTxt, cardTxt;
-    private static JButton orderButton;
+public class Order extends JFrame {
+    private JLabel orderTxt, addressInfoTxt, cardInfoTxt;
+    private JTextField address1, address2, zipcode, city, state;
+    private JTextField cardName, cardNum, CVV;
+    private JButton orderButton;
 
-    static String[] addressTxtArray = new String[]{"address1", "address2", "zipcode", "city", "state"};
-    static JTextField[] addressBoxArray = new JTextField[]{address1, address2, zipcode, city, state};
+    String[] addressTxtArray = new String[]{"Address 1", "Address 2", "Zipcode", "City", "State"};
+    JTextField[] addressBoxArray = new JTextField[5];
 
-    static String[] cardTxtArray = new String[]{"Card Name", "Card Number", "CVV"};
-    static JTextField[] cardBoxArray = new JTextField[]{cardName, cardNum, CVV};
+    String[] cardTxtArray = new String[]{"Card Name", "Card Number", "CVV"};
+    JTextField[] cardBoxArray = new JTextField[3];
+
+    // The logo image
+    private ImageIcon logoIcon = new ImageIcon("Logo.png");
 
     public static void main(String[] args) {
-        DisplayOrderScreen();
+        new Order().DisplayOrderScreen();
     }
-    
-    public static void DisplayOrderScreen(){
 
-        JPanel panel = new JPanel();
+    public void DisplayOrderScreen() {
+
+        // Custom JPanel class to paint the background
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // Draw the image as the background of the panel
+                g.drawImage(logoIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        };
         panel.setLayout(null);
 
-        //display window
+        // Display window
         JFrame window = new JFrame();
-        window.setTitle("LOGIN PAGE");
+        window.setTitle("Order Page");
         window.setLocation(new Point(500, 300));
         window.add(panel);
         window.setSize(new Dimension(800, 900));
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //display "Order" Title
-        makeText(panel, orderTxt, "Order Page", 100, 8, 100, 20);
+        // Display "Order" Title
+        makeText(panel, "Order Page", 100, 60, 100, 20, Color.RED);
 
-        //display "Address Info" Title
-        makeText(panel, addressInfoTxt, "Address Info", 100, 40, 100, 20);
+        // Display "Address Info" Title
+        makeText(panel, "Address Info", 100, 100, 100, 20, Color.BLACK);
 
-        int posy = 90;
+        int posy = 150;
         int txtx = 140;
-        int boxx = txtx + 90;
+        int boxx = txtx + 110;
 
-        //display address text
-        for( int i = 0; i < 5; i++){
-            makeText(panel, addressTxt, addressTxtArray[i], txtx, posy, 100, 20);
-            posy= posy + 50;
+        // Display address text
+        for (int i = 0; i < addressTxtArray.length; i++) {
+            makeText(panel, addressTxtArray[i], txtx, posy, 100, 20, Color.WHITE);
+            makeBox(panel, addressBoxArray, i, boxx, posy, 193, 28);
+            posy = posy + 50;
         }
 
-        //display address box
-        posy = 90;
-        for(int i = 0; i < 5; i++){
-            makeBox(panel, addressBoxArray[i], boxx, posy, 193, 28);
-            posy= posy + 50;
+        // Display "Card Info"
+        makeText(panel, "Card Info", 100, 420, 100, 20, Color.RED);
+
+        // Display card text
+        posy = 470;
+        for (int i = 0; i < cardTxtArray.length; i++) {
+            makeText(panel, cardTxtArray[i], txtx, posy, 100, 20, Color.WHITE);
+            makeBox(panel, cardBoxArray, i, boxx, posy, 193, 28);
+            posy = posy + 50;
         }
 
-        //display "Card Info"
-        makeText(panel, cardInfoTxt, "Card Info", 100, 370, 100, 20);
+        // Add Purchase Button
+        orderButton = new JButton("Purchase");
+        orderButton.setBounds(350, 750, 100, 30);
+        orderButton.setBackground(Color.RED);
+        orderButton.setForeground(Color.BLACK);
+        orderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Show a popup message
+                JOptionPane.showMessageDialog(window, 
+                    "Your purchase was complete, your confirmation will be sent to your email address.",
+                    "Purchase Complete",
+                    JOptionPane.INFORMATION_MESSAGE);
 
-        //display card text
-        posy = 420;
-        for( int i = 0; i < 3; i++){
-            makeText(panel, cardTxt, cardTxtArray[i], txtx, posy, 100, 20);
-            posy= posy + 50;
-        }
-
-        //display card box
-        posy = 420;
-        for( int i = 0; i < 3; i++){
-            makeBox(panel, cardBoxArray[i], boxx, posy, 193, 28);
-            posy= posy + 50;
-        }
-
-        //update quantity
+                // Navigate to the HomePage
+                HomePage home = new HomePage(new ShoppingCart());
+                home.DisplayHomePage();
+            }
+        });
+        panel.add(orderButton);
 
         window.setVisible(true);
     }
 
-    static public void makeText(JPanel panel, JLabel txtVar, String txt, int x, int y, int width, int height){
-        txtVar = new JLabel(txt);
+    public void makeText(JPanel panel, String txt, int x, int y, int width, int height, Color color) {
+        JLabel txtVar = new JLabel(txt);
         txtVar.setBounds(x, y, width, height);
+        txtVar.setForeground(color);
         panel.add(txtVar);
     }
 
-    static public void makeBox(JPanel panel, JTextField boxVar, int x, int y, int width, int height){
-        boxVar = new JTextField();
+    public void makeBox(JPanel panel, JTextField[] boxArray, int index, int x, int y, int width, int height) {
+        JTextField boxVar = new JTextField();
         boxVar.setBounds(x, y, width, height);
+        boxArray[index] = boxVar;
         panel.add(boxVar);
     }
-    
 }
