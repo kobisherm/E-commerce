3b657edf8f73da863cefd2649ec6c90a1145c06e
Davit Sargsyan <davit.sargsyan.603@my.csun.edu>
2023-08-18 18:47:06 -0700

    Final test for User class

--- UserTest.java
+++ UserTest.java
@@ -4,38 +4,34 @@ import org.junit.Test;
 
 public class UserTest {
 
-    private User user;
-
-    @Before
-    public void setUp() {
-        user = new User("test@example.com", "Password@123");
-    }
-
     @Test
     public void testRegisterUserWithValidInput() {
+        User user = new User("test5@gmail.com", "Password@12354");
         assertTrue(user.registerUser());
     }
 
     @Test
     public void testRegisterUserWithInvalidEmail() {
-        user = new User("invalid-email", "Password@123");
+        User user = new User("invalid-email", "Password@123");
         assertFalse(user.registerUser());
     }
 
     @Test
     public void testRegisterUserWithWeakPassword() {
-        user = new User("test@example.com", "weakpass");
+        User user = new User("test@example.com", "weakpass");
         assertFalse(user.registerUser());
     }
 
     @Test
     public void testLoginWithValidCredentials() {
+        User user = new User("test@gmail.com", "Password@12354");
         user.registerUser();
         assertTrue(user.login());
     }
 
     @Test
     public void testLoginWithInvalidCredentials() {
+        User user = new User("ye", "hey");
         assertFalse(user.login());
     }
 }
